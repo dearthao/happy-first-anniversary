8f7926f37e6e3cef2ddefcee276d2e02063e7aa6
vietanhle97 <vietanhle0710@gmail.com>
2025-12-16 01:05:46 +0000

    update file

--- src/Postcard.tsx
+++ src/Postcard.tsx
@@ -11,14 +11,14 @@ import coverImagePath from './assets/bg.jpg';
 import img1 from './assets/1.jpg'; 
 import img2 from './assets/2.jpg'; 
 import img3 from './assets/3.jpg'; 
-import img4 from './assets/4.jpg'; 
-import img5 from './assets/5.jpg'; 
+import img4 from './assets/4.JPG'; 
+import img5 from './assets/5.JPG'; 
 import img6 from './assets/6.png'; 
 import img7 from './assets/7.jpg'; 
 import img8 from './assets/8.jpg'; 
 import img9 from './assets/9.jpg'; 
-import img10 from './assets/10.jpg';
-import img11 from './assets/11.jpg';  
+import img10 from './assets/10.JPG';
+import img11 from './assets/11.JPG';  
 import img12 from './assets/12.jpg';  
 
 const galleryImages = [
